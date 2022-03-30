82b02bc7badba50890827738c2c4518ed8789a99
Anthony Shellman <atshellman@gmail.com>
2022-03-29 20:30:48 -0600

    Add annotations

--- variables.js
+++ variables.js
@@ -1,11 +1,13 @@
+// Strings
 var name = "Joeseph Joestar"
 var anime = JoJo's Bizarre Adventure"
 var reaction = "Oh My God!!"
 
+// Numbers
 var evenNumber = 22
 var oddNumber = 44
 var threeDigitNumber = 777
 
-
+// Booleans
 var likesPizza = true
 var likesAnchovies = false
