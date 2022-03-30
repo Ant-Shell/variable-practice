ea93631d50dee7be58ac103230ce9315b35f20a8
Anthony Shellman <atshellman@gmail.com>
2022-03-29 20:29:40 -0600

    Update variable names and add booleans

--- variables.js
+++ variables.js
@@ -2,6 +2,10 @@ var name = "Joeseph Joestar"
 var anime = JoJo's Bizarre Adventure"
 var reaction = "Oh My God!!"
 
-var number_1 = 22
-var number_2 = 44
-var number_3 = 66
+var evenNumber = 22
+var oddNumber = 44
+var threeDigitNumber = 777
+
+
+var likesPizza = true
+var likesAnchovies = false
